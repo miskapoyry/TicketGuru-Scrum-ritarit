be3d35c0ccd31b701fc1dd505bed6829946a15c1
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-09-18 18:16:03 +0300

    Lisaa sale entiteetti

--- src/main/java/ticketguru/domain/Sale.java
+++ src/main/java/ticketguru/domain/Sale.java
@@ -13,4 +13,78 @@ public class Sale {
     
     @Column(name="ticket_id", nullable = false)
     private Long ticketId;
+
+    @Column(name="sold_by", nullable = false)
+    private Long Id;
+
+    @Column(name="sale_timestamp", nullable = false)
+    private String saleTimestamp;
+
+    @Column(name="payment_method", nullable = false)
+    private String paymentMethod;
+
+    @Column(name="total_price", nullable = false)
+    private int totalPrice;
+
+    public Sale() {
+    }
+
+    public Sale(Long eventId, Long ticketId, Long id, String saleTimestamp, String paymentMethod, int totalPrice) {
+        this.eventId = eventId;
+        this.ticketId = ticketId;
+        Id = id;
+        this.saleTimestamp = saleTimestamp;
+        this.paymentMethod = paymentMethod;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getSaleTimestamp() {
+        return saleTimestamp;
+    }
+
+    public void setSaleTimestamp(String saleTimestamp) {
+        this.saleTimestamp = saleTimestamp;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    
 }
