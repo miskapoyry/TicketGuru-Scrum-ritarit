57b4e17a7432ca5d7ce1f70d779862ce646ec207
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-09-18 16:23:32 +0300

    AppUser repository luotu

--- src/main/java/ticketguru/domain/AppUserRepository.java
+++ src/main/java/ticketguru/domain/AppUserRepository.java
@@ -1,5 +1,9 @@
 package ticketguru.domain;
 
-public class AppUserRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppUserRepository extends CrudRepository<AppUser,Long> {
+
+    AppUser findByUsername(String username); //Tämä vain täytteenä tulevaisuutta varten
     
 }
