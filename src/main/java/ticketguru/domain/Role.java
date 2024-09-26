501e23811decb8bb931d3111912ba5d075c09698
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-09-26 15:15:37 +0300

    Luo role entiteetti

---
+++ src/main/java/ticketguru/domain/Role.java
@@ -0,0 +1,54 @@
+package ticketguru.domain;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false, updatable = false)
+    private Long roleId;
+
+    @Column(name = "role_name", nullable = false)
+    private String roleName;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AppUser> users;
+
+    public Role() {
+    }
+
+    public Role(Long roleId, String roleName, List<AppUser> users) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+        this.users = users;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public List<AppUser> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<AppUser> users) {
+        this.users = users;
+    }
+}
