50c724c1fc3b77260a7707d15808592d03aae563
Jesse224 <jeze363@gmail.com>
2024-10-02 16:21:34 +0300

    Lisätty RoleDTO

---
+++ src/main/java/ticketguru/DTO/RoleDTO.java
@@ -0,0 +1,31 @@
+package ticketguru.DTO;
+
+public class RoleDTO {
+
+    private Long roleId;
+    private String roleName;
+
+    public RoleDTO() {
+    }
+
+    public RoleDTO(Long roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
