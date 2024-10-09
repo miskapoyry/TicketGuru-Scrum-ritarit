d0c5d174bfa0842fdfacd8b849e7ae58382b2f5d
Pekka Naatsaari <pekka.naatsaari@hotmail.com>
2024-10-10 01:42:12 +0300

    Konstruktori ticketTypeId luontiin

--- src/main/java/ticketguru/domain/TicketType.java
+++ src/main/java/ticketguru/domain/TicketType.java
@@ -1,34 +1,38 @@
 package ticketguru.domain;
 
-import java.util.*;
-
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "ticket_type")
 public class TicketType {
 
+    // Unique identifier for the ticket type
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="ticket_type_id", nullable = false, updatable = false)
+    @Column(name = "ticket_type_id", nullable = false, updatable = false)
     private Long ticketTypeId;
 
+    // Name of the ticket type
     @Column(name = "ticket_type_name", nullable = false)
     private String ticketTypeName;
 
-    //@OneToMany(mappedBy = "ticketType", cascade = CascadeType.ALL, orphanRemoval = true)
-    //private List<EventTicketType> eventTicketTypes;
-    
-    //@OneToMany(mappedBy = "ticketType", cascade = CascadeType.ALL, orphanRemoval = true)
-    //private List<Ticket> tickets;
 
+    
+    // Default constructor
     public TicketType() {
     }
 
+    // Constructor to initialize all fields
     public TicketType(String ticketTypeName) {
         this.ticketTypeName = ticketTypeName;
     }
 
+    // Constructor with ticketTypeId
+    public TicketType(Long ticketTypeId) {
+        this.ticketTypeId = ticketTypeId;
+    }
+
+    //  Getters and setters
     public Long getTicketTypeId() {
         return ticketTypeId;
     }
@@ -44,20 +48,4 @@ public class TicketType {
     public void setTicketTypeName(String ticketTypeName) {
         this.ticketTypeName = ticketTypeName;
     }
-
-//    public List<EventTicketType> getEventTicketTypes() {
-//         return eventTicketTypes;
-//     }
-
-//     public void setEventTicketTypes(List<EventTicketType> eventTicketTypes) {
-//      this.eventTicketTypes = eventTicketTypes;
-//     }
-
-    // public List<Ticket> getTickets() {
-    //     return tickets;
-    // }
-
-    // public void setTickets(List<Ticket> tickets) {
-    //     this.tickets = tickets;
-    // }
 }
