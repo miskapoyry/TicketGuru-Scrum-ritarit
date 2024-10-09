0f651aa7cb5059884ccbfa2b443b55f5220a4770
Pekka Naatsaari <pekka.naatsaari@hotmail.com>
2024-10-10 02:19:35 +0300

    TicketType jaettu service ja resource luokkiin

--- src/main/java/ticketguru/web/rest/TicketTypeController.java
+++ src/main/java/ticketguru/web/rest/TicketTypeController.java
@@ -1,76 +1,59 @@
 package ticketguru.web.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ticketguru.domain.TicketType;
 import ticketguru.DTO.TicketTypeDTO;
-import ticketguru.repository.TicketTypeRepository;
+import ticketguru.service.TicketTypeService;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/ticket-types")
 public class TicketTypeController {
 
-    private final TicketTypeRepository ticketTypeRepository;
-
-    public TicketTypeController(TicketTypeRepository ticketTypeRepository) {
-        this.ticketTypeRepository = ticketTypeRepository;
-    }
+    @Autowired
+    private TicketTypeService ticketTypeService;
 
     // GET all ticket types
     @GetMapping
     public List<TicketTypeDTO> getAllTicketTypes() {
-        List<TicketType> ticketTypes = ticketTypeRepository.findAll();
-        return ticketTypes.stream()
-                          .map(this::toDTO)
-                          .collect(Collectors.toList());
+        return ticketTypeService.getAllTicketTypes();
     }
 
     // GET ticket type by ID
     @GetMapping("/{id}")
     public ResponseEntity<TicketTypeDTO> getTicketTypeById(@PathVariable Long id) {
-        return ticketTypeRepository.findById(id)
-                .map(ticketType -> ResponseEntity.ok(toDTO(ticketType)))
-                .orElse(ResponseEntity.notFound().build());
+        Optional<TicketTypeDTO> ticketType = ticketTypeService.getTicketTypeById(id);
+        return ticketType.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     // POST a new ticket type
-    @PostMapping(consumes = {"application/json"})
+    @PostMapping(consumes = { "application/json" })
     public ResponseEntity<TicketTypeDTO> createTicketType(@RequestBody TicketTypeDTO ticketTypeDTO) {
-        TicketType ticketType = new TicketType(ticketTypeDTO.getTicketTypeName());
-        TicketType savedTicketType = ticketTypeRepository.save(ticketType);
-        return ResponseEntity.ok(toDTO(savedTicketType));
+        TicketTypeDTO createdTicketType = ticketTypeService.createTicketType(ticketTypeDTO);
+        return ResponseEntity.ok(createdTicketType);
     }
 
     // DELETE a ticket type by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTicketType(@PathVariable Long id) {
-        if (!ticketTypeRepository.existsById(id)) {
+        try {
+            ticketTypeService.deleteTicketType(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
-        ticketTypeRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
     }
 
     // PATCH to update a ticket type
     @PatchMapping("/{id}")
-    public ResponseEntity<TicketTypeDTO> updateTicketType(@PathVariable Long id, @RequestBody TicketTypeDTO ticketTypeDTO) {
-        return ticketTypeRepository.findById(id)
-                .map(existingTicketType -> {
-                    existingTicketType.setTicketTypeName(ticketTypeDTO.getTicketTypeName());
-                    TicketType updatedTicketType = ticketTypeRepository.save(existingTicketType);
-                    return ResponseEntity.ok(toDTO(updatedTicketType));
-                })
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    // Convert TicketType to TicketTypeDTO
-    private TicketTypeDTO toDTO(TicketType ticketType) {
-        return new TicketTypeDTO(
-                ticketType.getTicketTypeId(),
-                ticketType.getTicketTypeName()
-        );
+    public ResponseEntity<TicketTypeDTO> updateTicketType(@PathVariable Long id,
+            @RequestBody TicketTypeDTO ticketTypeDTO) {
+        Optional<TicketTypeDTO> updatedTicketType = ticketTypeService.updateTicketType(id, ticketTypeDTO);
+        return updatedTicketType.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
