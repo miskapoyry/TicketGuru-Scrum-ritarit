0a728a244877055532079647715fdbbb28e150e1
Pekka Naatsaari <pekka.naatsaari@hotmail.com>
2024-10-10 01:40:46 +0300

    Ticket_number korvattu uuid:lla

--- src/main/java/ticketguru/domain/Ticket.java
+++ src/main/java/ticketguru/domain/Ticket.java
@@ -1,6 +1,7 @@
 package ticketguru.domain;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -10,39 +11,50 @@ import jakarta.persistence.*;
 @Table(name = "ticket")
 public class Ticket {
 
+    // Unique identifier for the ticket
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ticket_id", nullable = false, updatable = false)
     private Long ticketId;
 
+    // Unique ticket number
     @Column(name = "ticket_number", nullable = false, unique = true)
     private String ticketNumber;
 
+    // Many to one relationship with Event
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonBackReference
     @JoinColumn(name="event_id", nullable = false)
     private Event event;
 
+    // Many to one relationship with TicketType
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="ticket_type_id", nullable = false)
     private TicketType ticketType;
 
+    // Many to one relationship with Sale
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="sale_id", nullable = false)
     private Sale sale;
 
+    // Date and time of the sale
     @Column(name = "sale_timestamp", nullable = false)
     private Timestamp saleTimestamp;
 
+    // Boolean to check if the ticket is used
     @Column(name = "is_used", nullable = false)
     private boolean isUsed;
 
+    // Date and time of the ticket being used
     @Column(name = "used_timestamp")
     private Timestamp usedTimestamp;
 
+    // Default constructor
     public Ticket() {
+        this.ticketNumber = UUID.randomUUID().toString();
     }
 
+    // Constructor to initialize all fields
     public Ticket(String ticketNumber, Event event, TicketType ticketType, Sale sale, Timestamp saleTimestamp,
             boolean isUsed, Timestamp usedTimestamp) {
         this.ticketNumber = ticketNumber;
@@ -54,6 +66,7 @@ public class Ticket {
         this.usedTimestamp = usedTimestamp;
     }
 
+    // Getters and setters
     public String getTicketNumber() {
         return ticketNumber;
     }
@@ -70,12 +83,20 @@ public class Ticket {
         this.event = event;
     }
 
+    public Long getEventId() {
+        return event != null ? event.getEventId() : null;
+    }
+
     public TicketType getTicketType() {
         return ticketType;
     }
 
     public void setTicketType(TicketType ticketType) {
         this.ticketType = ticketType;
+    }
+
+    public Long getTicketTypeId() {
+        return ticketType != null ? ticketType.getTicketTypeId() : null;
     }
 
     public Sale getSale() {
@@ -117,4 +138,5 @@ public class Ticket {
     public void setTicketId(Long ticketId) {
         this.ticketId = ticketId;
     }
+
 }
