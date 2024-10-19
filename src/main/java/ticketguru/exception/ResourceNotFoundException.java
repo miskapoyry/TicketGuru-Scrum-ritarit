a1a501efdf61370fc37a377f5ea444a6caf96542
Hanriiel <hannariikka.happonen@gmail.com>
2024-10-19 16:34:42 +0300

    Exception-package ja luokat ErrorResponse & ResourceNotFoundException luotu

---
+++ src/main/java/ticketguru/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ticketguru.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
