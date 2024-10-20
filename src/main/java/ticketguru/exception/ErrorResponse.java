62a3246bf7b2ea3770911dc3ce4373515e515f10
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-10-21 00:14:56 +0300

    Lisaa tietotyyppeja, jotta API selkeampi

--- src/main/java/ticketguru/exception/ErrorResponse.java
+++ src/main/java/ticketguru/exception/ErrorResponse.java
@@ -1,10 +1,23 @@
 package ticketguru.exception;
 
+import java.time.LocalDateTime;
+
 public class ErrorResponse {
     private String message;
+    private int status;
+    private LocalDateTime timestamp;
+    private String path;
 
     public ErrorResponse(String message) {
         this.message = message;
+    }
+
+    // Lisätty bodyyn lisätietoja messagen lisäksi
+    public ErrorResponse(String message, int status, LocalDateTime timestamp, String path) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = timestamp;
+        this.path = path;
     }
 
     public String getMessage() {
@@ -13,5 +26,29 @@ public class ErrorResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
