0148bdf736289961784e0425922b46224e915899
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-10-28 16:44:29 +0200

    Poista turha import

--- src/main/java/ticketguru/service/EventService.java
+++ src/main/java/ticketguru/service/EventService.java
@@ -1,7 +1,6 @@
 package ticketguru.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import ticketguru.DTO.EventDTO;
