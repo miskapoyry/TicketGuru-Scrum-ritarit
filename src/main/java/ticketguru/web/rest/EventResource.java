b28ef06bc83dbf6b1df8bba9e9f4c68d65d570e2
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-10-29 13:00:01 +0200

    Poista turha import

--- src/main/java/ticketguru/web/rest/EventResource.java
+++ src/main/java/ticketguru/web/rest/EventResource.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import ticketguru.domain.Event;
 import ticketguru.exception.ResourceNotFoundException;
 import ticketguru.service.EventService;
 import ticketguru.DTO.EventDTO;
