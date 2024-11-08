f6b5a666c6fd4544cfd7d4e0a0a0c1b439ec3aa4
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-11-08 10:01:45 +0200

    lisaa nat20 palvelimen osoite corsiin

--- src/main/java/ticketguru/security/SecurityConfig.java
+++ src/main/java/ticketguru/security/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
         CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
             configuration
-                    .setAllowedOrigins(Arrays.asList("https://ticket-guru-ticketguru-scrum-ritarit.2.rahtiapp.fi","http://localhost:8080","http://127.0.0.1:5500/"));
+                    .setAllowedOrigins(Arrays.asList("https://ticket-guru-ticketguru-scrum-ritarit.2.rahtiapp.fi","http://localhost:8080","http://127.0.0.1:5500/","https://ticket-client.hellmanstudios.fi"));
             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "*"));
             configuration.setAllowCredentials(true);
